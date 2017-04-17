0d4c5360f051ea0ca7dfd97b3c36fb59b120eb0d
melissa pringle <melissa.a.acree@gmail.com>
2017-04-17 18:52:32 -0500

    error will not go away after several attemtps

--- src/App.js
+++ src/App.js
@@ -10,42 +10,10 @@ class App extends Component {
           <div className="title">
             <p>GA Instruments TI-WDI</p>
           </div>
-          <div className="display">
-            <input type="text" readOnly size="18" id="d"/>
+           <div className="display">
           </div>
           <div className="keys">
-            <p>
-              <input type="button" className="button gray" value="mrc" />
-              <input type="button" className="button gray" value="m-" />
-              <input type="button" className="button gray" value="m+" />
-              <input type="button" className="button pink" value="/" />
-            </p>
-            <p>
-            <input type="button" className="button black" value="7" />
-              <input type="button" className="button black" value="8" />
-                <input type="button" className="button black" value="9" />
-                <input type="button" className="button pink" value="*" />
-              </p>
-              <p>
-                <input type="button" className="button black" value="4" />
-                <input type="button" className="button black" value="5" />
-                  <input type="button" className="button black" value="6" />
-                  <input type="button" className="button pink" value="-" />
-                  </p>
-                <p>
-                  <input type="button" className="button black" value="1" />
-                <input type="button" className="button black" value="2" />
-                  <input type="button" className="button black" value="3" />
-                  <input type="button" className="button pink" value="+" />
 
-                  </p>
-                <p>
-                  <input type="button" className="button black" value="0" />
-                <input type="button" className="button black" value="." />
-                  <input type="button" className="button black" value="C" />
-                  <input type="button" className="button orange" value="=" />
-
-                  </p>
               </div>
             </div>
           </div>
